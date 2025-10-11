6b8ddfa40e98bd1f503b34dca98cb92f2855baa5
Rafał Kalata <01032763@pw.edu.pl>
2025-10-11 23:36:21 +0200

    add new projects, classes

--- InvoiceCreateSystem.BuildTests/BuildVerificationTests.cs
+++ InvoiceCreateSystem.BuildTests/BuildVerificationTests.cs
@@ -1,40 +1,42 @@
-﻿using System.Diagnostics;
-using Xunit;
+﻿namespace InvoiceCreateSystem.BuildTests;
 
-namespace InvoiceCreateSystem.BuildTests
+using System.Diagnostics;
+
+public class BuildVerificationTests
 {
-    public class BuildVerificationTests
+    [Fact]
+    public void AllProjects_Should_Build_Successfully()
     {
-        [Fact]
-        public void AllProjects_Should_Build_Successfully()
+        var projectsToBuild = new[]
+        {            
+            @"..\..\..\..\CreateInvoiceSystem.Address\CreateInvoiceSystem.Address.csproj",
+            @"..\..\..\..\CreateInvoiceSystem.Clients\CreateInvoiceSystem.Clients.csproj",
+            @"..\..\..\..\CreateInvoiceSystem.InvoicePositions\CreateInvoiceSystem.InvoicePositions.csproj",
+            @"..\..\..\..\CreateInvoiceSystem.Invoices\CreateInvoiceSystem.Invoices.csproj",
+            @"..\..\..\..\CreateInvoiceSystem.MethodsOfPayments\CreateInvoiceSystem.MethodsOfPayments.csproj",
+            @"..\..\..\..\CreateInvoiceSystem.Persistence\CreateInvoiceSystem.Persistence.csproj",
+            @"..\..\..\..\CreateInvoiceSystem.Products\CreateInvoiceSystem.Products.csproj",
+            @"..\..\..\..\CreateInvoiceSystem.SharedKernel\CreateInvoiceSystem.SharedKernel.csproj",
+            @"..\..\..\..\CreateInvoiceSystem.Web\CreateInvoiceSystem.Web.csproj"
+
+        };
+
+        foreach (var project in projectsToBuild)
         {
-            var projectsToBuild = new[]
-            {
-                @"..\..\..\..\ClientService.API\ClientService.API.csproj",
-                @"..\..\..\..\ClientService.Application\ClientService.Application.csproj",
-                @"..\..\..\..\ClientService.Domain\ClientService.Domain.csproj",
-                @"..\..\..\..\ClientService.Infrastructure\ClientService.Infrastructure.csproj",
-                @"..\..\..\..\CreateInvoiceSystem.Blazor\CreateInvoiceSystem.Blazor.csproj"                
-            };
+            var process = new Process();
+            process.StartInfo.FileName = "dotnet";
+            process.StartInfo.Arguments = $"build \"{project}\"";
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
 
-            foreach (var project in projectsToBuild)
-            {
-                var process = new Process();
-                process.StartInfo.FileName = "dotnet";
-                process.StartInfo.Arguments = $"build \"{project}\"";
-                process.StartInfo.RedirectStandardOutput = true;
-                process.StartInfo.RedirectStandardError = true;
-                process.StartInfo.UseShellExecute = false;
-                process.StartInfo.CreateNoWindow = true;
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+            process.WaitForExit();
 
-                process.Start();
-                string output = process.StandardOutput.ReadToEnd();
-                string error = process.StandardError.ReadToEnd();
-                process.WaitForExit();
-
-                Assert.True(process.ExitCode == 0, $"Build failed for {project}:\n{output}\n{error}");
-            }
+            Assert.True(process.ExitCode == 0, $"Build failed for {project}:\n{output}\n{error}");
         }
-
     }
 }
