cf5cd96c7ea15f91fa7f698144b1c4ed10e4ca31
Rafał Kalata <01032763@pw.edu.pl>
2025-11-15 17:05:53 +0100

    move validation fron entity to dto

--- CreateInvoiceSystem.Address/Domain/Entities/Address.cs
+++ CreateInvoiceSystem.Address/Domain/Entities/Address.cs
@@ -12,21 +12,13 @@ public class Address(int addressId, string street, string number, string city, s
 
     public int AddressId { get; set; } = addressId;
 
-    [Required]
-    [Display(Name = "Ulica")]
-    [MaxLength(255)]
+    
     public string Street { get; set; } = street;
-    [Required]
-    [Display(Name = "Numer")]
-    [MaxLength(255)]
+    
     public string Number { get; set; } = number;
-    [Required]
-    [Display(Name = "Miejscowość")]
-    [MaxLength(255)]
+    
     public string City { get; set; } = city;
-    [Required]
-    [Display(Name = "kod pocztowy")]
-    [MaxLength(255)]
+    
     public string PostalCode { get; set; } = postalCode;
     public string Email { get; set; } = email;
     //public ICollection<Client> Clients { get; set; }
