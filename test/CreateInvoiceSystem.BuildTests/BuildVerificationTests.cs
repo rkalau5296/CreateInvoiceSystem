8543753edba43272017dbe9707c7a6dfa917966c
Rafał Kalata <01032763@pw.edu.pl>
2025-11-21 13:47:49 +0100

    change file structure

---
+++ test/CreateInvoiceSystem.BuildTests/BuildVerificationTests.cs
@@ -0,0 +1,44 @@
+﻿namespace InvoiceCreateSystem.BuildTests;
+
+using System.Diagnostics;
+
+public class BuildVerificationTests
+{
+    [Fact]
+    public void AllProjects_Should_Build_Successfully()
+    {
+        var projectsToBuild = new[]
+        {
+            @"..\..\..\..\..\src\CreateInvoiceSystem.Abstractions\CreateInvoiceSystem.Abstractions.csproj",
+            @"..\..\..\..\..\src\CreateInvoiceSystem.Addresses\CreateInvoiceSystem.Addresses.csproj",
+            @"..\..\..\..\..\src\CreateInvoiceSystem.API\CreateInvoiceSystem.API.csproj",
+            @"..\..\..\..\..\src\CreateInvoiceSystem.Clients\CreateInvoiceSystem.Clients.csproj",
+            @"..\..\..\..\..\src\CreateInvoiceSystem.Identity\CreateInvoiceSystem.Identity.csproj",
+            @"..\..\..\..\..\src\CreateInvoiceSystem.InvoicePositions\CreateInvoiceSystem.InvoicePositions.csproj",
+            @"..\..\..\..\..\src\CreateInvoiceSystem.Invoices\CreateInvoiceSystem.Invoices.csproj",
+            @"..\..\..\..\..\src\CreateInvoiceSystem.MethodsOfPayments\CreateInvoiceSystem.MethodsOfPayments.csproj",
+            @"..\..\..\..\..\src\CreateInvoiceSystem.Nbp\CreateInvoiceSystem.Nbp.csproj",
+            @"..\..\..\..\..\src\CreateInvoiceSystem.Persistence\CreateInvoiceSystem.Persistence.csproj",
+            @"..\..\..\..\..\src\CreateInvoiceSystem.Products\CreateInvoiceSystem.Products.csproj",
+            @"..\..\..\..\..\src\CreateInvoiceSystem.Web\CreateInvoiceSystem.Web.csproj"
+        };
+
+        foreach (var project in projectsToBuild)
+        {
+            var process = new Process();
+            process.StartInfo.FileName = "dotnet";
+            process.StartInfo.Arguments = $"build \"{project}\"";
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+            process.WaitForExit();
+
+            Assert.True(process.ExitCode == 0, $"Build failed for {project}:\n{output}\n{error}");
+        }
+    }
+}
