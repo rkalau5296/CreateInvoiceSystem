c88f59a05e6256ed959325417ba6c982dcd075c8
Rafał Kalata <01032763@pw.edu.pl>
2025-11-23 01:46:23 +0100

    fix client validators

--- src/CreateInvoiceSystem.Clients/Application/Validators/UpdateClientRequestValidator.cs
+++ src/CreateInvoiceSystem.Clients/Application/Validators/UpdateClientRequestValidator.cs
@@ -18,22 +18,27 @@ public class UpdateClientRequestValidator : AbstractValidator<Client>
         RuleFor(x => x.AddressId)
             .GreaterThan(0).WithMessage("Address is required.");
 
-        RuleFor(x => x.UserId)
-            .GreaterThan(0).WithMessage("UserId is required.");
+        //RuleFor(x => x.UserId)
+        //    .GreaterThan(0).WithMessage("UserId is required.");
 
         RuleFor(x => x.Address)
             .NotNull().WithMessage("Address must be specified.");
-
-        // Validate AddressBase properties
+        
         When(x => x.Address != null, () =>
         {
             RuleFor(x => x.Address.Street)
                 .NotEmpty().WithMessage("Street is required in address.");
 
+            RuleFor(x => x.Address.Number)
+                .NotEmpty().WithMessage("Street number is required in address.");
+
             RuleFor(x => x.Address.City)
                 .NotEmpty().WithMessage("City is required in address.");
 
             RuleFor(x => x.Address.PostalCode)
+                .NotEmpty().WithMessage("Postal code is required in address.");
+
+            RuleFor(x => x.Address.Country)
                 .NotEmpty().WithMessage("Postal code is required in address.");
         });
     }
