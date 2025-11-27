976fb910c56b65d9a1c662a56e92d3c5c87242bf
Rafał Kalata <01032763@pw.edu.pl>
2025-11-27 16:11:56 +0100

    add invoice and user controllers

---
+++ src/CreateInvoiceSystem.API/Controllers/InvoiceController.cs
@@ -0,0 +1,62 @@
+﻿namespace CreateInvoiceSystem.API.Controllers;
+
+using CreateInvoiceSystem.Abstractions.ControllerBase;
+using CreateInvoiceSystem.Abstractions.Dto;
+using CreateInvoiceSystem.Invoices.Application.RequestsResponses.CreateInvoice;
+using CreateInvoiceSystem.Invoices.Application.RequestsResponses.DeleteInvoice;
+using CreateInvoiceSystem.Invoices.Application.RequestsResponses.GetInvoice;
+using CreateInvoiceSystem.Invoices.Application.RequestsResponses.GetInvoices;
+using CreateInvoiceSystem.Invoices.Application.RequestsResponses.UpdateInvoice;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class InvoiceController : ApiControllerBase
+{
+    public InvoiceController(IMediator mediator, ILogger<InvoiceController> logger) : base(mediator)
+    {
+        logger.LogInformation("This is AddressController");
+    }
+
+    [HttpGet("{InvoiceId}")]
+    [ProducesResponseType(typeof(GetInvoiceResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public Task<IActionResult> GetInvoiceAsync([FromRoute] int InvoiceId, CancellationToken cancellationToken)
+    {
+        GetInvoiceRequest request = new(InvoiceId);
+        return this.HandleRequest<GetInvoiceRequest, GetInvoiceResponse>(request, cancellationToken);
+    }
+
+    [HttpGet()]
+    [Route("/Invoices")]
+    public async Task<IActionResult> GetInvoicesAsync([FromQuery] GetInvoicesRequest request, CancellationToken cancellationToken)
+    {
+        return await this.HandleRequest<GetInvoicesRequest, GetInvoicesResponse>(request, cancellationToken);
+    }
+
+    [HttpPost]
+    [Route("create")]
+    public async Task<IActionResult> CreateInvoicesAsync([FromBody] InvoiceDto InvoiceDto, CancellationToken cancellationToken)
+    {
+        CreateInvoiceRequest request = new(InvoiceDto);
+        return await this.HandleRequest<CreateInvoiceRequest, CreateInvoiceResponse>(request, cancellationToken);
+    }
+
+    [HttpPut]
+    [Route("update/id")]
+    public async Task<IActionResult> UpdateInvoiceAsync(int id, [FromBody] InvoiceDto InvoiceDto, CancellationToken cancellationToken)
+    {
+        UpdateInvoiceRequest request = new(id, InvoiceDto);
+        return await this.HandleRequest<UpdateInvoiceRequest, UpdateInvoiceResponse>(request, cancellationToken);
+    }
+
+    [HttpDelete]
+    [Route("id")]
+    public async Task<IActionResult> DeleteInvoice(int id, CancellationToken cancellationToken)
+    {
+        DeleteInvoiceRequest request = new(id);
+        return await this.HandleRequest<DeleteInvoiceRequest, DeleteInvoiceResponse>(request, cancellationToken);
+    }
+
+}
---
+++ src/CreateInvoiceSystem.API/Controllers/UserController.cs
@@ -0,0 +1,62 @@
+﻿namespace CreateInvoiceSystem.API.Controllers;
+
+using CreateInvoiceSystem.Abstractions.ControllerBase;
+using CreateInvoiceSystem.Abstractions.Dto;
+using CreateInvoiceSystem.Users.Application.RequestsResponses.CreateUser;
+using CreateInvoiceSystem.Users.Application.RequestsResponses.DeleteUser;
+using CreateInvoiceSystem.Users.Application.RequestsResponses.GetUser;
+using CreateInvoiceSystem.Users.Application.RequestsResponses.GetUsers;
+using CreateInvoiceSystem.Users.Application.RequestsResponses.UpdateUser;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ApiControllerBase
+{
+    public UserController(IMediator mediator, ILogger<UserController> logger) : base(mediator)
+    {
+        logger.LogInformation("This is AddressController");
+    }
+
+    [HttpGet("{UserId}")]
+    [ProducesResponseType(typeof(GetUserResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public Task<IActionResult> GetUserAsync([FromRoute] int UserId, CancellationToken cancellationToken)
+    {
+        GetUserRequest request = new(UserId);
+        return this.HandleRequest<GetUserRequest, GetUserResponse>(request, cancellationToken);
+    }
+
+    [HttpGet()]
+    [Route("/Users")]
+    public async Task<IActionResult> GetUsersAsync([FromQuery] GetUsersRequest request, CancellationToken cancellationToken)
+    {
+        return await this.HandleRequest<GetUsersRequest, GetUsersResponse>(request, cancellationToken);
+    }
+
+    [HttpPost]
+    [Route("create")]
+    public async Task<IActionResult> CreateUsersAsync([FromBody] UserDto UserDto, CancellationToken cancellationToken)
+    {
+        CreateUserRequest request = new(UserDto);
+        return await this.HandleRequest<CreateUserRequest, CreateUserResponse>(request, cancellationToken);
+    }
+
+    [HttpPut]
+    [Route("update/id")]
+    public async Task<IActionResult> UpdateUserAsync(int id, [FromBody] UserDto UserDto, CancellationToken cancellationToken)
+    {
+        UpdateUserRequest request = new(id, UserDto);
+        return await this.HandleRequest<UpdateUserRequest, UpdateUserResponse>(request, cancellationToken);
+    }
+
+    [HttpDelete]
+    [Route("id")]
+    public async Task<IActionResult> DeleteUser(int id, CancellationToken cancellationToken)
+    {
+        DeleteUserRequest request = new(id);
+        return await this.HandleRequest<DeleteUserRequest, DeleteUserResponse>(request, cancellationToken);
+    }
+
+}
