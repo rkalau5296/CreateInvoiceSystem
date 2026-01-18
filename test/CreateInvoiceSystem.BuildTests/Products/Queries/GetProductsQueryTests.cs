fd732e39fc7b372797034ffb9ec5b91f1eb26eba
Rafał Kalata <01032763@pw.edu.pl>
2026-01-19 00:12:21 +0100

    add products tests

---
+++ test/CreateInvoiceSystem.BuildTests/Products/Queries/GetProductsQueryTests.cs
@@ -0,0 +1,99 @@
+﻿using CreateInvoiceSystem.Modules.Products.Domain.Application.Queries;
+using CreateInvoiceSystem.Modules.Products.Domain.Entities;
+using CreateInvoiceSystem.Modules.Products.Domain.Interfaces;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace CreateInvoiceSystem.BuildTests.Products.Queries;
+
+public class GetProductsQueryTests
+{
+    private readonly Mock<IProductRepository> _repositoryMock;
+
+    public GetProductsQueryTests()
+    {
+        _repositoryMock = new Mock<IProductRepository>();
+    }
+
+    [Fact]
+    public async Task Execute_ShouldReturnListOfProducts_WhenProductsExist()
+    {
+        // Arrange
+        var userId = 123;
+        var query = new GetProductsQuery(userId);
+        var expectedProducts = new List<Product>
+        {
+            new() { ProductId = 1, Name = "Produkt A", UserId = userId },
+            new() { ProductId = 2, Name = "Produkt B", UserId = userId }
+        };
+
+        _repositoryMock
+            .Setup(r => r.GetAllAsync(userId, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(expectedProducts);
+
+        // Act
+        var result = await query.Execute(_repositoryMock.Object, CancellationToken.None);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().HaveCount(2);
+        result.Should().BeEquivalentTo(expectedProducts);
+
+        _repositoryMock.Verify(r => r.GetAllAsync(userId, It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task Execute_ShouldThrowInvalidOperationException_WhenRepositoryReturnsNull()
+    {
+        // Arrange
+        var userId = 1;
+        var query = new GetProductsQuery(userId);
+        
+        _repositoryMock
+            .Setup(r => r.GetAllAsync(It.IsAny<int?>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((List<Product>)null!);
+
+        // Act
+        Func<Task> act = async () => await query.Execute(_repositoryMock.Object, CancellationToken.None);
+
+        // Assert
+        await act.Should().ThrowAsync<InvalidOperationException>()
+            .WithMessage("List of products is empty.");
+    }
+
+    [Fact]
+    public async Task Execute_ShouldReturnEmptyList_WhenRepositoryReturnsEmptyList()
+    {
+        // Arrange
+        var query = new GetProductsQuery(null);
+        var emptyList = new List<Product>();
+
+        _repositoryMock
+            .Setup(r => r.GetAllAsync(null, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(emptyList);
+
+        // Act
+        var result = await query.Execute(_repositoryMock.Object, CancellationToken.None);
+
+        // Assert        
+        result.Should().NotBeNull();
+        result.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task Execute_ShouldHandleNullUserId()
+    {
+        // Arrange
+        var query = new GetProductsQuery(null);
+        _repositoryMock
+            .Setup(r => r.GetAllAsync(null, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new List<Product> { new() { ProductId = 1 } });
+
+        // Act
+        await query.Execute(_repositoryMock.Object, CancellationToken.None);
+
+        // Assert
+        _repositoryMock.Verify(r => r.GetAllAsync(null, It.IsAny<CancellationToken>()), Times.Once);
+    }
+}
---
+++ test/CreateInvoiceSystem.BuildTests/Products/Validators/CreateProductRequestValidatorTests.cs
@@ -0,0 +1,110 @@
+﻿using CreateInvoiceSystem.Modules.Products.Domain.Application.RequestsResponses.CreateProduct;
+using CreateInvoiceSystem.Modules.Products.Domain.Application.Validators;
+using CreateInvoiceSystem.Modules.Products.Domain.Dto;
+using FluentValidation.TestHelper;
+using Xunit;
+
+namespace CreateInvoiceSystem.BuildTests.Products.Validators;
+
+public class CreateProductRequestValidatorTests
+{
+    private readonly CreateProductRequestValidator _validator;
+
+    public CreateProductRequestValidatorTests()
+    {
+        _validator = new CreateProductRequestValidator();
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_Name_Is_Empty()
+    {
+        // Arrange
+        var dto = new CreateProductDto("", "Description", 10.00m, 1);
+        var request = new CreateProductRequest(dto);
+
+        // Act & Assert
+        var result = _validator.TestValidate(request);
+        result.ShouldHaveValidationErrorFor(x => x.Product.Name)
+              .WithErrorMessage("Name is required.");
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_Name_Exceeds_100_Characters()
+    {
+        // Arrange
+        var dto = new CreateProductDto(new string('A', 101), "Description", 10.00m, 1);
+        var request = new CreateProductRequest(dto);
+
+        // Act & Assert
+        var result = _validator.TestValidate(request);
+        result.ShouldHaveValidationErrorFor(x => x.Product.Name);
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_UserId_Is_Zero_Or_Less()
+    {
+        // Arrange
+        var dto = new CreateProductDto("Test", "Description", 10.00m, 0);
+        var request = new CreateProductRequest(dto);
+
+        // Act & Assert
+        var result = _validator.TestValidate(request);
+        result.ShouldHaveValidationErrorFor(x => x.Product.UserId)
+              .WithErrorMessage("UserId is required.");
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_Value_Is_Null()
+    {
+        // Arrange
+        var dto = new CreateProductDto("Test", "Description", null, 1);
+        var request = new CreateProductRequest(dto);
+
+        // Act & Assert
+        var result = _validator.TestValidate(request);
+        result.ShouldHaveValidationErrorFor(x => x.Product.Value)
+              .WithErrorMessage("Value is required.");
+    }
+
+    [Theory]
+    [InlineData(10.555)] 
+    [InlineData(10.1234)]
+    public void Should_Have_Error_When_Value_Has_More_Than_Two_Decimal_Places(decimal invalidValue)
+    {
+        // Arrange
+        var dto = new CreateProductDto("Test", "Description", invalidValue, 1);
+        var request = new CreateProductRequest(dto);
+
+        // Act & Assert
+        var result = _validator.TestValidate(request);
+        result.ShouldHaveValidationErrorFor(x => x.Product.Value)
+              .WithErrorMessage("Value must be a decimal with max 2 digits after the decimal point.");
+    }
+
+    [Theory]
+    [InlineData(10)]
+    [InlineData(10.5)]
+    [InlineData(10.55)]
+    public void Should_Not_Have_Error_When_Value_Is_Correct(decimal validValue)
+    {
+        // Arrange
+        var dto = new CreateProductDto("Test", "Description", validValue, 1);
+        var request = new CreateProductRequest(dto);
+
+        // Act & Assert
+        var result = _validator.TestValidate(request);
+        result.ShouldNotHaveValidationErrorFor(x => x.Product.Value);
+    }
+
+    [Fact]
+    public void Should_Not_Have_Errors_When_Request_Is_Valid()
+    {
+        // Arrange
+        var dto = new CreateProductDto("Valid Product", "Good Description", 99.99m, 1);
+        var request = new CreateProductRequest(dto);
+
+        // Act & Assert
+        var result = _validator.TestValidate(request);
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+}
