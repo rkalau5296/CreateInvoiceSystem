be180371ecdcb12d5ca8dbde200398a24b738b31
Rafał Kalata <01032763@pw.edu.pl>
2026-02-13 14:42:50 +0100

    add validate components

---
+++ src/CreateInvoiceSystem.Frontend/Components/BootstrapValidationClassProvider.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace CreateInvoiceSystem.Frontend.Components;
+
+public class BootstrapValidationClassProvider : FieldCssClassProvider
+{
+    public override string GetFieldCssClass(EditContext editContext, in FieldIdentifier fieldIdentifier)
+    {
+        var hasErrors = editContext.GetValidationMessages(fieldIdentifier).Any();
+        if (!editContext.IsModified(fieldIdentifier))
+        {
+            return string.Empty;
+        }
+        return hasErrors ? "is-invalid" : "is-valid";
+    }
+}
