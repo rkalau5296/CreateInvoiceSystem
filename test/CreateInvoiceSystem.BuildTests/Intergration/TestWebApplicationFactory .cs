d1c709a72f818e7f2c85e4707e39e99a8c718a7b
Rafał Kalata <01032763@pw.edu.pl>
2026-04-21 09:32:43 +0200

    add jwt configuration in tests

--- test/CreateInvoiceSystem.BuildTests/Intergration/TestWebApplicationFactory .cs
+++ test/CreateInvoiceSystem.BuildTests/Intergration/TestWebApplicationFactory .cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
 
@@ -19,6 +20,17 @@ public class TestWebApplicationFactory : WebApplicationFactory<Program>
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+        builder.ConfigureAppConfiguration((context, config) =>
+        {
+            config.AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                ["Jwt:Key"] = "TwojBardzoDlugiISuperTajnyKluczDoGenerowaniaTokenow123!",
+                ["Jwt:Issuer"] = "CreateInvoiceSystem",
+                ["Jwt:Audience"] = "CreateInvoiceSystemUsers",
+                ["Jwt:ExpiryMinutes"] = "15"
+            });
+        });
+
         builder.ConfigureServices(services =>
         {
             var toRemove = services.Where(d =>
