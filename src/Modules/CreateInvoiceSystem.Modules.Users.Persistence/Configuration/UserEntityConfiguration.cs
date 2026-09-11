97853fe94ff8b1717335d830c70527b656401f79
Rafał Kalata <01032763@pw.edu.pl>
2026-09-12 00:25:10 +0200

    move folders and files into proper localization

--- src/Modules/CreateInvoiceSystem.Modules.Users.Persistence/Configuration/UserEntityConfiguration.cs
+++ src/Modules/CreateInvoiceSystem.Modules.Users.Persistence/Configuration/UserEntityConfiguration.cs
@@ -1,5 +1,4 @@
 ﻿using CreateInvoiceSystem.Modules.Users.Persistence.Entities;
-using CreateInvoiceSystem.Shared.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System.Reflection.Emit;
