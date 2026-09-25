4331a248b7ee3df94f42ccb1811d822ff6889aba
Rafał Kalata <01032763@pw.edu.pl>
2026-09-25 12:16:10 +0200

    remove UpdateClientCommand

--- src/Modules/CreateInvoiceSystem.Modules.Clients.Domain/Application/Handlers/UpdateClientHandler.cs
+++ src/Modules/CreateInvoiceSystem.Modules.Clients.Domain/Application/Handlers/UpdateClientHandler.cs
@@ -1,21 +1,60 @@
-﻿using CreateInvoiceSystem.Abstractions.Executors;
-using CreateInvoiceSystem.Modules.Clients.Domain.Application.Commands;
-using CreateInvoiceSystem.Modules.Clients.Domain.Application.RequestsResponses.UpdateClient;
+﻿using CreateInvoiceSystem.Modules.Clients.Domain.Application.RequestsResponses.UpdateClient;
+using CreateInvoiceSystem.Modules.Clients.Domain.Entities;
 using CreateInvoiceSystem.Modules.Clients.Domain.Interfaces;
+using CreateInvoiceSystem.Modules.Clients.Domain.Mappers;
 using MediatR;
 
 namespace CreateInvoiceSystem.Modules.Clients.Domain.Application.Handlers;
-public class UpdateClientHandler(ICommandExecutor commandExecutor, IClientRepository _clientRepository) : IRequestHandler<UpdateClientRequest, UpdateClientResponse>
+public class UpdateClientHandler(IClientRepository _clientRepository) : IRequestHandler<UpdateClientRequest, UpdateClientResponse>
 {    
     public async Task<UpdateClientResponse> Handle(UpdateClientRequest request, CancellationToken cancellationToken)
-    {        
-        var command = new UpdateClientCommand() { Parametr = request.Client };
-        
-        var client = await commandExecutor.Execute(command, _clientRepository, cancellationToken);        
+    {
+        ArgumentNullException.ThrowIfNull(request.Client);
+
+        var client = await _clientRepository.GetByIdAsync(request.Client.ClientId, request.Client.UserId, cancellationToken)
+            ?? throw new InvalidOperationException(
+                $"Client with ID {request.Client.ClientId} not found.");
+
+        client.Name = request.Client.Name ?? client.Name;
+        client.Nip = request.Client.Nip ?? client.Nip;
+        client.Email = request.Client.Email ?? client.Email;
+
+        if (client.Address is null && request.Client.Address is not null)
+        {
+            client.Address = new Address
+            {
+                Street = request.Client.Address.Street,
+                Number = request.Client.Address.Number,
+                City = request.Client.Address.City,
+                PostalCode = request.Client.Address.PostalCode,
+                Country = request.Client.Address.Country
+            };
+        }
+        else if (client.Address is not null && request.Client.Address is not null)
+        {
+            client.Address.Street =
+                request.Client.Address.Street ?? client.Address.Street;
+
+            client.Address.Number =
+                request.Client.Address.Number ?? client.Address.Number;
+
+            client.Address.City =
+                request.Client.Address.City ?? client.Address.City;
+
+            client.Address.PostalCode =
+                request.Client.Address.PostalCode ?? client.Address.PostalCode;
+
+            client.Address.Country =
+                request.Client.Address.Country ?? client.Address.Country;
+        }
+
+        var updatedClient = await _clientRepository.UpdateAsync(
+            client,
+            cancellationToken);        
 
         return new UpdateClientResponse()
         {
-            Data = client
+            Data = ClientMappers.ToUpdateDto(updatedClient)
         };
     }
 }
--- test/CreateInvoiceSystem.BuildTests/Unit/UpdateClientHandlerTests.cs
+++ test/CreateInvoiceSystem.BuildTests/Unit/UpdateClientHandlerTests.cs
@@ -1,9 +1,9 @@
 ﻿using CreateInvoiceSystem.BuildTests.Base;
 using CreateInvoiceSystem.Modules.Clients.Domain.Application.Handlers;
 using CreateInvoiceSystem.Modules.Clients.Domain.Application.RequestsResponses.UpdateClient;
-using CreateInvoiceSystem.Modules.Clients.Domain.Application.Commands;
-using CreateInvoiceSystem.Modules.Clients.Domain.Interfaces;
 using CreateInvoiceSystem.Modules.Clients.Domain.Dto;
+using CreateInvoiceSystem.Modules.Clients.Domain.Entities;
+using CreateInvoiceSystem.Modules.Clients.Domain.Interfaces;
 using FluentAssertions;
 using Moq;
 
@@ -15,82 +15,138 @@ public class UpdateClientHandlerTests : BaseTest<IClientRepository>
 
     public UpdateClientHandlerTests()
     {
-        _sut = new UpdateClientHandler(ExecutorMock.Object, RepositoryMock.Object);
+        _sut = new UpdateClientHandler(RepositoryMock.Object);
     }
 
     [Fact]
-    public async Task Handle_ShouldReturnData_WhenUpdateIsSuccessful()
+    public async Task Handle_ShouldUpdateClientAndReturnResponse_WhenDataIsValid()
     {
         // Arrange
-        var clientId = 1;
-        var address = new AddressDto(50, "Testowa", "1", "Miasto", "00-000", "Polska");
-        var updateDto = new UpdateClientDto(0, "Firma", "123", address, 50, 100, "testc@test.com");
-        var request = new UpdateClientRequest(updateDto, clientId);
-        
-        var expectedResult = updateDto with { ClientId = clientId };
+        var addressDto = new AddressDto(
+            5,
+            "Nowa Ulica",
+            "2",
+            "Gdynia",
+            "81-000",
+            "Polska");
 
-        ExecutorMock.Setup(e => e.Execute(
-                It.IsAny<UpdateClientCommand>(),
-                RepositoryMock.Object,
-                CancellationToken))
-            .ReturnsAsync(expectedResult);
+        var updateDto = new UpdateClientDto(
+            1,
+            "Zaktualizowany Klient",
+            "9876543210",
+            addressDto,
+            5,
+            10,
+            "testc@test.com");
+
+        var request = new UpdateClientRequest(updateDto, 1);
+
+        var existingClient = new Client
+        {
+            ClientId = 1,
+            UserId = 10,
+            Name = "Stary Klient",
+            Nip = "0000000000",
+            AddressId = 5,
+            Address = new Address
+            {
+                AddressId = 5,
+                Street = "Stara Ulica",
+                Number = "1",
+                City = "Gdańsk",
+                PostalCode = "80-000",
+                Country = "Polska"
+            }
+        };
+
+        RepositoryMock
+            .Setup(r => r.GetByIdAsync(1, 10, CancellationToken))
+            .ReturnsAsync(existingClient);
+
+        RepositoryMock
+            .Setup(r => r.UpdateAsync(It.IsAny<Client>(), CancellationToken))
+            .ReturnsAsync(existingClient);
 
         // Act
         var result = await _sut.Handle(request, CancellationToken);
 
         // Assert
         result.Should().NotBeNull();
-        result.Data.Should().BeEquivalentTo(expectedResult);
+        result.Data.Should().NotBeNull();
+        result.Data!.Name.Should().Be("Zaktualizowany Klient");
+        result.Data.Address!.Street.Should().Be("Nowa Ulica");
+
+        RepositoryMock.Verify(
+            r => r.GetByIdAsync(1, 10, CancellationToken),
+            Times.Once);
+
+        RepositoryMock.Verify(
+            r => r.UpdateAsync(
+                It.Is<Client>(client =>
+                    client.ClientId == 1
+                    && client.UserId == 10
+                    && client.Name == "Zaktualizowany Klient"
+                    && client.Nip == "9876543210"
+                    && client.Email == "testc@test.com"
+                    && client.Address!.Street == "Nowa Ulica"
+                    && client.Address.Number == "2"
+                    && client.Address.City == "Gdynia"
+                    && client.Address.PostalCode == "81-000"
+                    && client.Address.Country == "Polska"),
+                CancellationToken),
+            Times.Once);
+
+        RepositoryMock.VerifyNoOtherCalls();
     }
 
     [Fact]
-    public async Task Handle_ShouldPassCancellationTokenToExecutor()
+    public async Task Handle_ShouldCreateNewAddress_WhenExistingClientHasNoAddress()
     {
         // Arrange
-        var address = new AddressDto(1, "", "", "", "", "");
-        var updateDto = new UpdateClientDto(1, "", "", address, 1, 1, "testc@test.com");
+        var addressDto = new AddressDto(0, "Nowa Ulica", "1", "Warszawa", "00-001", "Polska");
+        var updateDto = new UpdateClientDto(1, "Klient", "123", addressDto, 0, 1, "testc@test.com");
         var request = new UpdateClientRequest(updateDto, 1);
-        using var cts = new CancellationTokenSource();
 
-        ExecutorMock.Setup(e => e.Execute(
-                It.IsAny<UpdateClientCommand>(),
-                RepositoryMock.Object,
-                cts.Token))
-            .ReturnsAsync(updateDto);
+        var existingClient = new Client { ClientId = 1, UserId = 1, Name = "Klient", Address = null! };
+
+        RepositoryMock
+            .Setup(r => r.GetByIdAsync(1, 1, CancellationToken))
+            .ReturnsAsync(existingClient);
+
+        RepositoryMock
+            .Setup(r => r.UpdateAsync(It.IsAny<Client>(), CancellationToken))
+            .ReturnsAsync(existingClient);
 
         // Act
-        await _sut.Handle(request, cts.Token);
+        await _sut.Handle(request, CancellationToken);
 
         // Assert
-        ExecutorMock.Verify(e => e.Execute(
-            It.IsAny<UpdateClientCommand>(),
-            RepositoryMock.Object,
-            cts.Token), Times.Once);
+        existingClient.Address.Should().NotBeNull();
+        existingClient.Address!.Street.Should().Be("Nowa Ulica");
+        existingClient.Address.City.Should().Be("Warszawa");
     }
 
     [Fact]
-    public async Task Handle_ShouldPropagateException_WhenExecutorThrows()
+    public async Task Handle_ShouldThrowInvalidOperationException_WhenClientNotFound()
     {
         // Arrange
-        var address = new AddressDto(1, "", "", "", "", "");
-        var updateDto = new UpdateClientDto(1, "", "", address, 1, 1, "testc@test.com");
-        var request = new UpdateClientRequest(updateDto, 1);
+        var updateDto = new UpdateClientDto(99, "Nieistniejący", "123", null!, 0, 1, "testc@test.com");
+        var request = new UpdateClientRequest(updateDto, 99);
 
-        ExecutorMock.Setup(e => e.Execute(
-                It.IsAny<UpdateClientCommand>(),
-                RepositoryMock.Object,
-                CancellationToken))
-            .ThrowsAsync(new Exception("Błąd bazy danych"));
+        RepositoryMock
+            .Setup(r => r.GetByIdAsync(99, 1, CancellationToken))
+            .ReturnsAsync((Client)null!);
 
         // Act
-        var act = async () => await _sut.Handle(request, CancellationToken);
+        Func<Task> act = async () => await _sut.Handle(request, CancellationToken);
 
         // Assert
-        await act.Should().ThrowAsync<Exception>();
-    }
+        await act.Should().ThrowAsync<InvalidOperationException>()
+            .WithMessage("Client with ID 99 not found.");
+    }    
 
     [Fact]
-    public void Constructor_ShouldThrowArgumentOutOfRangeException_WhenIdIsLessThanOne()
+    public void RequestConstructor_ShouldThrowArgumentOutOfRangeException_WhenIdIsLessThanOne()
     {
         // Arrange
         var address = new AddressDto(1, "", "", "", "", "");
@@ -105,7 +161,7 @@ public class UpdateClientHandlerTests : BaseTest<IClientRepository>
     }
 
     [Fact]
-    public void Constructor_ShouldThrowArgumentNullException_WhenClientDtoIsNull()
+    public void RequestConstructor_ShouldThrowArgumentNullException_WhenClientDtoIsNull()
     {
         // Act
         Action act = () => new UpdateClientRequest(null!, 1);
